6ffb8c823602925e7ae9eb55837bdce32ac48750
Jacob Williams <ponyboy47@gmail.com>
2019-07-02 14:00:06 -0600

    Correct the Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,26 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
-let package = Package(name: "ConfigParser",
-                      products: [
-                          // Products define the executables and libraries produced by a package, and make them visible to other packages.
-                          .library(name: "ConfigParser",
-                                   targets: ["ConfigParser"])
-                      ],
-                      dependencies: [
-                          // Dependencies declare other packages that this package depends on.
-                          // .package(url: /* package url */, from: "1.0.0"),
-                          .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.16.0")
-                      ],
-                      targets: [
-                          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                          // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-                          .target(name: "ConfigParser",
-                                  dependencies: ["TrailBlazer"]),
-                          .testTarget(name: "ConfigParserTests",
-                                      dependencies: ["ConfigParser"])
-                      ])
+let package = Package(
+    name: "ConfigParser",
+    products: [
+        .library(
+            name: "ConfigParser",
+            targets: ["ConfigParser"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.16.0"),
+    ],
+    targets: [
+        .target(
+            name: "ConfigParser",
+            dependencies: ["TrailBlazer"]
+        ),
+        .testTarget(
+            name: "ConfigParserTests",
+            dependencies: ["ConfigParser"]
+        ),
+    ]
+)
